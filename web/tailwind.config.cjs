97484700693ed1b5dd5a05bcfd65b214716a8bd1
N9neK <ll.nine.kouzuki.ll@gmail.com>
2022-09-14 14:22:36 -0300

    Inicializando repositório.

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // '''./(dentro da pasta)''' src/ '''**(todas as outras pastas que tiverem aqui dentro)''' /'''*.tsx(todos os arquivos que terminarem com tsx)'''
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-grandient':
+          'linear-gradient(90deg, #9572FC 0%, #43E7AD 50.00%, #E1D55D 100%)',
+          'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
+      }
+    }
+  },
+  plugins: []
+}
